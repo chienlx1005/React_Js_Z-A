ff483ab5d625f65c63dadb6d60a67343516ab050
chienlx1005 <chienlx1005@gmail.com>
2022-05-04 15:50:32 +0700

    mounted& unmounted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Content from "./Content";
+import "./App.css";
+
+
 
 function App() {
+  const [show,setShow] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <button onClick={() => setShow(!show)}>toggle</button>
+      {show && <Content/>}
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,11 @@
+
+function Content(){
+
+    return(
+        <div>
+            hi ae!
+        </div>
+    )
+}
+
+export default Content;

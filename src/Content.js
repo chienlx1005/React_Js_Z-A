6fa604ff01adab1a1188949422998d8c0d42c92d
chienlx1005 <chienlx1005@gmail.com>
2022-05-04 16:09:08 +0700

    show Preview Avatar

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 
 function Content(){
+    const [avt,setAvt] = useState();
+
+    useEffect(( ) => {
+        return()=> {
+            avt && URL.revokeObjectURL(avt.pre)
+        }
+    },[avt])
+
+    const handleImg = (e) => {
+
+        const file = e.target.files[0]
+        file.pre  = URL.createObjectURL(file)
+
+        setAvt(file)
+    }
 
     return(
         <div>
-            hi ae!
+           <input
+           type="file"
+           onChange={handleImg}
+           />
+           {avt && (<img src={avt.pre} alt="" width="80%"/>)}
         </div>
     )
 }
